f43712cce237c10ee833149dc26355b8fc0394c5
James Saunders <james@saunders-family.net>
2019-05-12 08:34:20 +0000

    Update batch files.

--- minerd.bat
+++ minerd.bat
@@ -1,1 +1,1 @@
-minerd-sse2.exe -a yescrypt -o stratum+tcp://206.189.78.140:7032 -u BKsaUAz6QDn5crJMbvYB8HSPdUX52j2kVA -p x
+minerd-sse2.exe -a yescrypt -o stratum+tcp://206.189.78.140:7032 -u BKsaUAz6QDn5crJMbvYB8HSPdUX52j2kVA -p xxxx
